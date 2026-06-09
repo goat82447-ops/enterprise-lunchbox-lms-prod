a61a0870b3d9f0e22a04920c781ec3eac707c423
goat82447-ops <goat82447@gmail.com>
2026-06-09 14:58:23 +0530

    Add Travel and Payment routes with guards

--- Frontend/lunchbox-app/src/app/app.routes.ts
+++ Frontend/lunchbox-app/src/app/app.routes.ts
@@ -11,6 +11,8 @@ import { CaptainProfileComponent } from './features/captain-profile/captain-prof
 import { AboutUsComponent } from './features/about-us/about-us.component';
 import { ContactComponent } from './features/contact/contact.component';
 import { ServicesComponent } from './core/services/services.component';
+import { TravelComponent } from './features/travel/travel.component';
+import { PaymentComponent } from './features/payment/payment.component';
 import { CartComponent } from './components/cart/cart.component';
 import { OrderComponent } from './components/order/order.component';
 import { TrackingComponent as OrderTrackingComponent } from './components/tracking/tracking.component';
@@ -25,8 +27,38 @@ export const routes: Routes = [
 	{ path: 'login', component: LoginComponent },
 	{ path: 'register', component: RegisterComponent },
 	{ path: 'services', component: ServicesComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'travel', component: TravelComponent, canActivate: [authGuard, customerGuard] },
 	{ path: 'activity', component: TrackingComponent, canActivate: [authGuard] },
 	{ path: 'account', component: AccountComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'payment', component: PaymentComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'about-us', component: AboutUsComponent },
+	{ path: 'contact', component: ContactComponent },
+	{ path: 'lunchbox-delivery', component: BookingComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'school-booking', component: BookingComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'booking/food/hotels', component: BookingComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'booking/food/menu/:hotelId', component: BookingComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'booking', component: BookingComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'cart', component: CartComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'order', component: OrderComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'order-tracking', component: OrderTrackingComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'order-tracking/:id', component: OrderTrackingComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'tracking', component: TrackingComponent, canActivate: [authGuard] },
+	{ path: 'tracking/:id', component: TrackingComponent, canActivate: [authGuard] },
+	{ path: 'captain-profile', component: CaptainProfileComponent, canActivate: [authGuard, captainGuard] },
+	{ path: 'admin', component: AdminComponent, canActivate: [authGuard, adminGuard] },
+	{ path: 'audit', component: AuditComponent, canActivate: [authGuard, adminGuard] },
+	{ path: '**', redirectTo: '/' }
+];
+
+export const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'home', component: HomeComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'register', component: RegisterComponent },
+	{ path: 'services', component: ServicesComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'activity', component: TrackingComponent, canActivate: [authGuard] },
+	{ path: 'account', component: AccountComponent, canActivate: [authGuard, customerGuard] },
+	{ path: 'payment', component: PaymentComponent, canActivate: [authGuard, customerGuard] },
 	{ path: 'about-us', component: AboutUsComponent },
 	{ path: 'contact', component: ContactComponent },
 	{ path: 'lunchbox-delivery', component: BookingComponent, canActivate: [authGuard, customerGuard] },
