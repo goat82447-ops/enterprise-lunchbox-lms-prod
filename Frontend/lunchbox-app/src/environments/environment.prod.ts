20e72226be511a9d80923a09d7fdd04b97cff146
goat82447-ops <goat82447@gmail.com>
2026-06-22 15:59:46 +0530

    Update environment.prod.ts

--- Frontend/lunchbox-app/src/environments/environment.prod.ts
+++ Frontend/lunchbox-app/src/environments/environment.prod.ts
@@ -1,15 +1,16 @@
 export const environment = {
  production: true,
   appVersion: '1.0.8',
-  // ForNode Prod
-  // authApiBase: 'https://ekart-backend-buwi.onrender.com',
-  // parcelApiBase: 'https://ekart-backend-buwi.onrender.com',
-  // gatewayApiBase: 'https://ekart-backend-buwi.onrender.com',
-  // webSocketUrl: 'wss://ekart-backend-buwi.onrender.com/ws',
-  authApiBase: 'https://dotnetroutix-server.onrender.com',
-  parcelApiBase: 'https://dotnetroutix-server.onrender.com',
-  gatewayApiBase: 'https://dotnetroutix-server.onrender.com',
-  webSocketUrl: 'wss://dotnetroutix-server.onrender.com/ws',
+  // ForNode Prod-Server
+   authApiBase: 'https://ekart-backend-buwi.onrender.com',
+   parcelApiBase: 'https://ekart-backend-buwi.onrender.com',
+   gatewayApiBase: 'https://ekart-backend-buwi.onrender.com',
+   webSocketUrl: 'wss://ekart-backend-buwi.onrender.com/ws',
+  //For Dotnet-Server
+  //authApiBase: 'https://dotnetroutix-server.onrender.com',
+  //parcelApiBase: 'https://dotnetroutix-server.onrender.com',
+  //gatewayApiBase: 'https://dotnetroutix-server.onrender.com',
+  //webSocketUrl: 'wss://dotnetroutix-server.onrender.com/ws',
   googleMapsApiKey: '',
   voiceBookingEnabled: true,
   liveTrackingEnabled: true,
